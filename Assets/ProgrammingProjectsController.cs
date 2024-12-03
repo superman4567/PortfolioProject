6239dbd2dd5ea722a0eef18c9befb1c13d7d6fd7
Nathan Steendam <bo2nathan@hotmail.com>
2024-12-03 08:49:57 +0100

    [ADD] Fork layout done

---
+++ Assets/ProgrammingProjectsController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgrammingProjectsController : MonoBehaviour
+{
+    [System.Serializable]
+    public class ButtonPanelPair
+    {
+        public Button button;
+        public GameObject panel;
+    }
+
+    public List<ButtonPanelPair> buttonPanelPairs;
+
+    private void Start()
+    {
+        foreach (var pair in buttonPanelPairs)
+        {
+            // Cache the reference for the panel to avoid using closures
+            var panelToActivate = pair.panel;
+
+            // Assign onClick listener for each button
+            pair.button.onClick.AddListener(() => ActivatePanel(panelToActivate));
+        }
+    }
+
+    private void ActivatePanel(GameObject panelToActivate)
+    {
+        // Loop through all panels and deactivate them
+        foreach (var pair in buttonPanelPairs)
+        {
+            if (pair.panel != null)
+                pair.panel.SetActive(false);
+        }
+
+        // Activate the selected panel
+        if (panelToActivate != null)
+            panelToActivate.SetActive(true);
+    }
+}
