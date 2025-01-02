c3356b9735deadde0e7b47dbbb1a2cfd44596898
Nathan Steendam <bo2nathan@hotmail.com>
2025-01-02 01:54:55 +0100

    [ADD] Small fix

--- Assets/SceneHandler.cs
+++ Assets/SceneHandler.cs
@@ -1,24 +1,34 @@
+using Cinemachine;
 using DG.Tweening;
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SceneHandler : MonoBehaviour
 {
     [Header("First Time Entering")]
-    [SerializeField] private CanvasGroup pressSpaceToStart;
+    [SerializeField] private CanvasGroup pressSpaceToStartCanvasGroup;
+    private Tween pressSpaceTween;
     [SerializeField] private Button pressSpaceToStartButton;
+
+    [Space]
+    [SerializeField] private CinemachineVirtualCamera introCamera;
     [SerializeField] private Transform startPosMM;
     [SerializeField] private Transform endPosMM;
+    [SerializeField] private float transitionDuration;
 
     [Header("Entering")]
+    [SerializeField] private CanvasGroup categoryCanvasGroup;
+    private Tween categoryTween;
     [SerializeField] private GameObject[] CategoryButtons;
-    
+
     void Start()
     {
-        ShowPressSpaceToStart();
-
         AssignOnclicks();
+        ShowPressSpaceToStart(false);
+        LerpCategoryOptions(true);
+
+        Transform cameraTransform = introCamera.transform;
+        cameraTransform.SetPositionAndRotation(startPosMM.position, startPosMM.rotation);
     }
 
     private void AssignOnclicks()
@@ -26,20 +36,55 @@ public class SceneHandler : MonoBehaviour
         pressSpaceToStartButton.onClick.AddListener(ShowCatergoryOptions);
     }
 
-    private void ShowPressSpaceToStart()
+    private void ShowPressSpaceToStart(bool stopTween)
     {
-        pressSpaceToStart.alpha = 1.0f;
-        pressSpaceToStart.DOFade(1f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-        pressSpaceToStart.interactable = true;
+        if (stopTween)
+        {
+            pressSpaceTween?.Kill();
+            pressSpaceToStartCanvasGroup.alpha = 0f;
+            pressSpaceToStartCanvasGroup.interactable = false;
+        }
+        else
+        {
+            pressSpaceToStartCanvasGroup.alpha = 0f;
+            pressSpaceTween = pressSpaceToStartCanvasGroup.DOFade(1f, 2f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+            pressSpaceToStartCanvasGroup.interactable = true;
+        }
     }
 
     private void ShowCatergoryOptions()
     {
-
+        LerpIntroCameraFirstTime();
+        ShowPressSpaceToStart(true); // Stop tween for pressSpaceToStartCanvasGroup
+        LerpCategoryOptions(false); // Show category options
     }
 
-    private void LerpToBase()
+    private void LerpCategoryOptions(bool stopTween)
     {
+        
 
+        if (stopTween)
+        {
+            categoryTween?.Kill(); // Stop the category fade tween
+            categoryCanvasGroup.alpha = 0f;
+            categoryCanvasGroup.interactable = false;
+        }
+        else
+        {
+            pressSpaceTween?.Kill();
+
+            categoryCanvasGroup.alpha = 0f;
+            categoryTween = categoryCanvasGroup.DOFade(1f, 1f).SetEase(Ease.InOutSine);
+            categoryCanvasGroup.interactable = true;
+        }
+    }
+
+    public void LerpIntroCameraFirstTime()
+    {
+        Transform cameraTransform = introCamera.transform;
+
+        cameraTransform.SetPositionAndRotation(startPosMM.position, startPosMM.rotation);
+        cameraTransform.DOMove(endPosMM.position, transitionDuration).SetEase(Ease.InOutSine);
+        cameraTransform.DORotateQuaternion(endPosMM.rotation, transitionDuration).SetEase(Ease.InOutSine);
     }
 }
