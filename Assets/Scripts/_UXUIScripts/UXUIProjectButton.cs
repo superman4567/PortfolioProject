54560468b46b78ef8e4f4928a993562373c03a82
Nathan Steendam <bo2nathan@hotmail.com>
2025-07-20 19:14:21 +0200

    [ADD] proper banners

--- Assets/Scripts/_UXUIScripts/UXUIProjectButton.cs
+++ Assets/Scripts/_UXUIScripts/UXUIProjectButton.cs
@@ -8,13 +8,23 @@ using TMPro;
 public class UXUIProjectButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private EnumUXUIProjects projectEnum;
+
+    [Space]
+    [Header("Button Components")]
     [SerializeField] private Button button;
-    [SerializeField] private TextMeshProUGUI buttontext;
+    [SerializeField] private TextMeshProUGUI buttonText;
+    [SerializeField] private Image buttonImage;
+
+    [Space]
+    [Header("Hover Effects")]
     [SerializeField] private float hoverScale = 1.05f;
     [SerializeField] private float tweenDuration = 0.2f;
+    [SerializeField] private Color normalColor = new Color(0xBA / 255f, 0xBA / 255f, 0xBA / 255f);
+    [SerializeField] private Color hoverColor = Color.white;
 
     private Vector3 originalScale;
-    private Tween currentTween;
+    private Tween scaleTween;
+    private Tween colorTween;
 
     public static event Action<EnumUXUIProjects> OnProjectButtonClicked;
     public static event Action<EnumUXUIProjects> OnProjectButtonHovered;
@@ -22,26 +32,44 @@ public class UXUIProjectButton : MonoBehaviour, IPointerEnterHandler, IPointerEx
     private void Awake()
     {
         originalScale = transform.localScale;
-        buttontext.text = projectEnum.ToString();
+        buttonText.text = projectEnum.ToString();
         button.onClick.AddListener(() => OnProjectButtonClicked?.Invoke(projectEnum));
     }
 
     private void OnDestroy()
     {
-        currentTween?.Kill();
+        scaleTween?.Kill();
+        colorTween?.Kill();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        currentTween?.Kill();
-        currentTween = transform.DOScale(originalScale * hoverScale, tweenDuration).SetEase(Ease.OutBack);
+        // scale up
+        scaleTween?.Kill();
+        scaleTween = transform
+            .DOScale(originalScale * hoverScale, tweenDuration)
+            .SetEase(Ease.OutBack);
+
+        // color to white
+        colorTween?.Kill();
+        colorTween = buttonImage
+            .DOColor(hoverColor, tweenDuration)
+            .SetEase(Ease.OutQuad);
+
         ChangeDisplayedProject();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        currentTween?.Kill();
-        currentTween = transform.DOScale(originalScale, tweenDuration).SetEase(Ease.OutBack);
+        // scale back
+        scaleTween?.Kill();
+        scaleTween = transform
+            .DOScale(originalScale, tweenDuration)
+            .SetEase(Ease.OutBack);
+
+        colorTween = buttonImage
+            .DOColor(normalColor, tweenDuration)
+            .SetEase(Ease.OutQuad);
     }
 
     private void ChangeDisplayedProject()
