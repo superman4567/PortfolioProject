f89852ba8be1dd3e7747b0e401ff0c5c1e1e1955
Nathan Steendam <bo2nathan@hotmail.com>
2025-07-22 22:45:37 +0200

    [FIX] Localization length

--- Assets/Scripts/_ThreeDScripts/ThreeDProjectInstructor.cs
+++ Assets/Scripts/_ThreeDScripts/ThreeDProjectInstructor.cs
@@ -7,6 +7,8 @@ public class ThreeDProjectInstructor : MonoBehaviour
 {
     [SerializeField] private CanvasGroup buttonContainer;
     [SerializeField] private CanvasGroup galleryContainer;
+    [SerializeField] private CanvasGroup returnButtonContainer;
+
 
     private readonly float transitionDelay = 0.1f;
 
@@ -31,6 +33,10 @@ public class ThreeDProjectInstructor : MonoBehaviour
         galleryContainer.alpha = 0;
         galleryContainer.interactable = false;
         galleryContainer.blocksRaycasts = false;
+
+        returnButtonContainer.alpha = 1;
+        returnButtonContainer.interactable = true;
+        returnButtonContainer.blocksRaycasts = true;
     }
 
     public void OnProjectButtonClicked_Callback(EnumThreeDProjects a)
@@ -45,6 +51,10 @@ public class ThreeDProjectInstructor : MonoBehaviour
 
     public IEnumerator OnThreeDProjectButtonClicked()
     {
+        returnButtonContainer.DOFade(0, 0.2f);
+        returnButtonContainer.interactable = false;
+        returnButtonContainer.blocksRaycasts = false;
+
         yield return new WaitForSeconds(transitionDelay); // Optional delay for smoother transition
 
         buttonContainer.DOFade(0, 0.5f).OnComplete(() =>
@@ -55,12 +65,15 @@ public class ThreeDProjectInstructor : MonoBehaviour
             galleryContainer.DOFade(1, 0.5f);
             galleryContainer.interactable = true;
             galleryContainer.blocksRaycasts = true;
-
         });
     }
 
     public IEnumerator OnBackToThreeDButtonList()
     {
+        returnButtonContainer.DOFade(1, 0.2f);
+        returnButtonContainer.interactable = true;
+        returnButtonContainer.blocksRaycasts = true;
+
         yield return new WaitForSeconds(transitionDelay);
 
         galleryContainer.DOFade(0, 0.5f).OnComplete(() =>
